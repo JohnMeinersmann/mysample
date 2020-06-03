6d4a9af1a79bab0bffef65328d2c37e5e04f8a78
John M <jsmeiners@yahoo.com>
2020-06-03 15:12:14 -0400

    Initial commit

---
+++ MyClass.java
@@ -0,0 +1,23 @@
+
+// cd Documents\Development\Java Lessons
+// javac MyClass.java
+// java MyClass
+public class MyClass {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+	String a = "65", b = "66", c = "67";
+	System.out.println(a + " " + b + " " + c);
+	System.out.println(b);
+	System.out.println(c);
+  }
+}
+
+public class JavaClass {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+	String a = "65", b = "66", c = "67";
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+  }
+}
